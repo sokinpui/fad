8a9170994fb55174be39d3a1edb197907d535211
sokinpui <sosokinpui@gmail.com>
2026-02-13 16:16:38 +0800

    add convert oklab

--- image.py
+++ image.py
@@ -20,3 +20,37 @@ def divide_into_patches(image: np.ndarray, patch_size: int) -> List[np.ndarray]:
             patches.append(patch)
 
     return patches
+
+
+def convert_to_oklab(image: np.ndarray) -> np.ndarray:
+    if image is None or image.size == 0:
+        return np.array([])
+
+    if len(image.shape) != 3 or image.shape[2] != 3:
+        return np.array([])
+
+    rgb = image.astype(np.float32) / 255.0
+    rgb = rgb[..., ::-1]
+
+    linear_rgb = np.where(
+        rgb > 0.04045,
+        ((rgb + 0.055) / 1.055) ** 2.4,
+        rgb / 12.92
+    )
+
+    rgb_to_lms = np.array([
+        [0.4122214708, 0.5363325363, 0.0514459929],
+        [0.2119034982, 0.6806995451, 0.1073969566],
+        [0.0883024619, 0.2817188376, 0.6299787005]
+    ])
+    lms = np.dot(linear_rgb, rgb_to_lms.T)
+
+    lms_non_linear = np.cbrt(lms)
+
+    lms_to_oklab = np.array([
+        [0.2104542553, 0.7936177850, -0.0040720468],
+        [1.9779984951, -2.4285922050, 0.4505937099],
+        [0.0259040371, 0.7827717662, -0.8086757660]
+    ])
+
+    return np.dot(lms_non_linear, lms_to_oklab.T)
