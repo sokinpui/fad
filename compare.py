b257c53807b753cabf968ee8053d7eaf0bb8e890
sokinpui <sosokinpui@gmail.com>
2026-02-13 12:18:14 +0800

    add compartor class

--- compare.py
+++ compare.py
@@ -1,28 +1,39 @@
 import argparse
+from abc import ABC, abstractmethod
+from typing import Any
 
 import cv2
 import numpy as np
 from skimage.metrics import structural_similarity as ssim
 
 
-def compare_ssim(image_a: np.ndarray, image_b: np.ndarray) -> float:
-    if image_a.shape != image_b.shape:
-        return 0.0
+class Comparetor(ABC):
+    @abstractmethod
+    def compare(self, image_a: Any, image_b: Any) -> Any:
+        pass
 
-    min_side = min(image_a.shape[:2])
-    if min_side < 7:
-        return 0.0
 
-    channel_axis = 2 if len(image_a.shape) == 3 else None
+class SSIMComparetor(Comparetor):
+    def compare(self, image_a: np.ndarray, image_b: np.ndarray) -> float:
+        if image_a.shape != image_b.shape:
+            return 0.0
 
-    # NOTE: range of score
-    # 1.0 perfectly match
-    # 0 no structural similarity
-    # -1.0 perfectly anti structural
-    return float(ssim(image_a, image_b, channel_axis=channel_axis))
+        min_side = min(image_a.shape[:2])
+        if min_side < 7:
+            return 0.0
+
+        channel_axis = 2 if len(image_a.shape) == 3 else None
+
+        # NOTE: range of score
+        # 1.0 perfectly match
+        # 0 no structural similarity
+        # -1.0 perfectly anti structural
+        return float(ssim(image_a, image_b, channel_axis=channel_axis))
 
 
 def run_comparison(baseline_path: str, test_path: str):
+
+    s = SSIMComparetor()
     image_a = cv2.imread(baseline_path)
     image_b = cv2.imread(test_path)
 
@@ -34,7 +45,7 @@ def run_comparison(baseline_path: str, test_path: str):
         print(f"Error: Could not read image at {test_path}")
         return
 
-    score = compare_ssim(image_a, image_b)
+    score = s.compare(image_a, image_b)
     print(f"SSIM: {score:.6f}")
 
 
